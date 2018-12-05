af71254521638bcbb3a5f3ee7c13cf4e97438772
murat <muratkhalilov289@gmail.com>
2018-12-05 12:37:04 +0300

    Add project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myshop/admin/');
+define('HTTP_CATALOG', 'http://myshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myshop/admin/');
+define('HTTPS_CATALOG', 'http://myshop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/myshop/admin/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/myshop/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/myshop/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/myshop/admin/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/myshop/admin/view/template/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/myshop/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/myshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/myshop/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/myshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/myshop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/myshop/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OSPanel/domains/myshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myshop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/myshop/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/myshop/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/myshop/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/myshop/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/myshop/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/myshop/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/myshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/myshop/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/myshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/myshop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/myshop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
